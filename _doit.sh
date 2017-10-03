0446789917827fcf620123c5e027ed182b97b758
Emory L <emory@incumbent.org>
2017-10-02 19:22:10 -0500

    spaces are tricky

--- _doit.sh
+++ _doit.sh
@@ -4,8 +4,10 @@ echo "bundle install time"
 bundle install
 echo "build the site using bundle exec jekyll"
 bundle exec jekyll build
+echo "readable"
+chmod -R ugo+r _site
 echo "put them in the document root for apache"
 rsync -azvP --delete _site/ /var/www/html/
 echo "make sure our permissions are sane-ish"
-find /var/www/html -type f -exec chmod ugo+r {}\;
-find /var/www/html -type d -exec chmod ugo+rx {}\;
+find /var/www/html -type f -exec chmod ugo+r {} \;
+find /var/www/html -type d -exec chmod ugo+rx {} \;
