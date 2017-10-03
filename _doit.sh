46a19a829d10bd21ef73ce5a5dca98cab899bf8e
Emory L <emory@incumbent.org>
2017-10-02 19:08:37 -0500

    aagaga

--- _doit.sh
+++ _doit.sh
@@ -4,8 +4,8 @@ echo "bundle install time"
 bundle install
 echo "build the site using bundle exec jekyll"
 bundle exec jekyll build
-echo "make sure our permissions are sane-ish"
-find _site -type f -exec chmod -R ugo+r {}\;
-find _site -type d -exec chmod ugo+rx {} \;
 echo "put them in the document root for apache"
 rsync -azvP --delete _site/ /var/www/html/
+echo "make sure our permissions are sane-ish"
+find /var/www/html -type f -exec chmod -R ugo+r {}\;
+find /var/www/html -type d -exec chmod ugo+rx {} \;
